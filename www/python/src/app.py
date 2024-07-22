b6b964f06f9b277fa490cf7688e62e6581d2dceb
Tiago Lubiana <tiago.lubiana.alves@usp.br>
2024-07-22 11:53:08 -0300

    feat: Change counter to sql database

--- www/python/src/app.py
+++ www/python/src/app.py
@@ -377,4 +377,5 @@ def plot_results(df):
 
 if __name__ == "__main__":
     init_db()
-    app.run(debug=True)
+    debug_mode = os.environ.get("FLASK_DEBUG", "False").lower() in ["true", "1", "t"]
+    app.run()
